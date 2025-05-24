1d4747796bb928b71e1f5990dab153d198d441e1
Chethana Dilhari <chethanadilhari99@gmail.com>
2025-05-25 00:57:39 +0530

    Home page header design

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,17 +1,41 @@
+
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Image, ScrollView, Text, View } from 'react-native';
 
 const home = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View className="flex-1 bg-white">
+      <View className="mb-5 " />
+      <Image
+      source={require('../../assets/headers/home.jpeg')}
+      className="absolute w-full z-0 h-64 rounded-3xl"
+      resizeMode="cover"
+      />
     
-      <Text className="text-teaGreen text-4xl font-inter-bold">PluckMate</Text>
+      <Image
+      source={require('../../assets/images/logo/logo.png')}
+      className="w-48 h-52 mt-10 mb-5 mx-auto"
+      style={{ tintColor: '#FFFFFF' }} 
+      />
+
+      <ScrollView
+      className="flex-1 px-5"
+      showsVerticalScrollIndicator={false}
+      contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}
+      >
+        <Text className="text-2xl font-bold text-center  text-teaGreen mb-5">
+          Welcome to the Home Page 
+        </Text>
+        <View
+          className="bg-lightTeaGreen rounded-2xl p-4 h-80 mb-5"
+          
+        >
+          <Text className="text-base text-gray-800 text-center">
+            This is a rectangle with rounded borders.
+          </Text>
+        </View>
+
+      </ScrollView>
     </View>
   );
 }

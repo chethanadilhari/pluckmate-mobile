62a648131ce2ce8828a18afe35a094753759ee51
Chethana Dilhari <chethanadilhari99@gmail.com>
2025-05-28 00:45:28 +0530

    Background color change in home pages

--- app/(tabs)/home/calendar.tsx
+++ app/(tabs)/home/calendar.tsx
@@ -3,11 +3,13 @@ import { Text, View } from 'react-native';
 
 const CalendarTab = () => {
   return (
+     <View style={{ flex: 1, backgroundColor: 'white' }}>
     <View className="bg-blue-100 rounded-2xl p-4 h-80 mb-5">
       <Text className="text-base text-gray-800 text-center">
         Calendar View will be shown here.
       </Text>
     </View>
+   </View>
   );
 };
 

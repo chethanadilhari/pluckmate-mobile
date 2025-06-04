75a93b46af8c860a03c7c0b81cdc4ab06816c8c3
tharindu <tharindu.builtapps@gmail.com>
2025-06-05 01:22:32 +0530

    add total employee count view card.

--- app/(tabs)/attendance.tsx
+++ app/(tabs)/attendance.tsx
@@ -1,13 +1,22 @@
 import DateTimePicker from '@react-native-community/datetimepicker';
 import { CalendarDays, ChevronDown, Search, } from 'lucide-react-native';
 import { useState } from 'react';
-import { Platform, Pressable, ScrollView, Text, TextInput, View } from 'react-native';
+import { Image, Platform, Pressable, ScrollView, Text, TextInput, View } from 'react-native';
 import Worker from '../../components/attendance/worker';
+
 
 
 const Attendance = () => {
   const [date, setDate] = useState(new Date());
   const [show, setShow] = useState(false);
+
+  const employees = [
+    { id: 1, name: 'John Doe', present: true },
+    { id: 2, name: 'Jane Smith', present: false },
+    { id: 3, name: 'Alice Johnson', present: true },
+    { id: 4, name: 'Bob Brown', present: true },
+    { id: 5, name: 'Charlie White', present: false },
+  ];
 
   const onChange = (event: any, selectedDate?: Date) => {
     const currentDate = selectedDate || date;
@@ -75,7 +84,7 @@ const Attendance = () => {
                 paddingVertical: 10,
               }}
               placeholder='Search Employee By Name...'
-              placeholderTextColor={'brown'}
+              placeholderTextColor={'Brown'}
             />
             <Search color="black" size={20} style={{ marginLeft: 8 }} />
           </View>
@@ -91,34 +100,83 @@ const Attendance = () => {
           <Text className="text-lg  text-gray-700 mt-4">Name</Text>
           <Text className="text-lg  text-gray-700 mt-4">Present/Absent</Text>
         </View>
-        <Worker />
-        <Worker />
-        <Worker />
-        <Worker />
-        <Worker />
+        {
+          employees.map((employee, index) => (
+            <Worker data={employee} key={employee.id} />
+          ))
+        }
         <View style={{ alignItems: 'center', marginTop: 20 }}>
           <Pressable
             className="bg-gray-400 px-6 py-2 rounded-xl"
-            onPress={() => {}}
+            onPress={() => { }}
           >
             <Text className="text-white font-semibold">Next &gt;&gt;</Text>
           </Pressable>
           <View className="flex-row space-x-4 mt-4 ">
             <Pressable
               className="bg-white px-6 py-2 rounded-xl border border-gray-400"
-              onPress={() => {}}
+              onPress={() => { }}
             >
               <Text className="text-green-800 font-semibold">Cancel</Text>
             </Pressable>
-            <Pressable 
+            <Pressable
               className="bg-green-800 px-6 py-2 rounded-xl ml-8 "
-              onPress={() => {}}
+              onPress={() => { }}
             >
               <Text className="text-white font-semibold">Save Changes</Text>
             </Pressable>
           </View>
         </View>
       </View>
+
+      <View >
+        <View style={{
+          padding: 16,
+          flex: 1,
+        }}
+        >
+          <View className='flex-row justify-between'>
+            <Text
+              style={{
+                fontSize: 14,
+                fontWeight: 'bold',
+                marginBottom: 8,
+                color: '#333',
+              }}
+              className="text-lg font-semibold text-gray-700 mt-4 ml-4">
+              Total Employee Count
+            </Text>
+            <View className="border-2 border-gray-400 bg-gray-100 rounded-2xl px-4 py-2 mt-2 mr-8">
+              <Text>
+                12
+              </Text>
+            </View>
+          </View>
+
+          <View>
+            <View className="flex-row border-2 border-gray-400 bg-gray-100 rounded-2xl px-4 py-4 mt-2 justify-between items-center">
+              <Text className="text-green-700 text-lg font-semibold">Present Count</Text>
+              <View style={{ width: 2, height: 32, backgroundColor: '#747474' }} />
+              <Text className="text-green-700 text-lg font-semibold">10</Text>
+            </View>
+
+            <View className="flex-row border-2 border-gray-400 bg-gray-100 rounded-2xl px-4 py-4 mt-2 justify-between items-center">
+              <Text className="text-green-700 text-lg font-semibold">Absent Count</Text>
+              <View style={{ width: 2, height: 32, backgroundColor: '#747474' }} />
+              <Text className="text-red-700 text-lg font-semibold">02</Text>
+            </View>
+          </View>
+        </View>
+        <View>
+          <Image
+            source={require('../../assets/images/tea_plucker.jpg')}
+            style={{ width: '100%', height: 200, borderRadius: 16, overflow: 'hidden' }}
+            resizeMode="cover"
+          >
+          </Image>
+        </View>
+      </View>
+
 
     </ScrollView>
   );
--- components/attendance/worker.tsx
+++ components/attendance/worker.tsx
@@ -1,32 +1,36 @@
 import React from 'react'
 import { View, Text, Pressable } from 'react-native'
 
-function Worker() {
+interface employeeRecord {
+  id: number;
+  name: string;
+  present: boolean;
+}
+const Worker = ({ data }: { data: employeeRecord }) => {
   return (
     <View className='flex-row items-center justify-between'
-              style={{
-                marginTop: 12,
-                width: '100%',
-                paddingHorizontal: 16,
-                paddingVertical: 3,
-                backgroundColor: 'white',
-                borderRadius: 8,
-    
-              }}>
-              <Text className="text-lg font-semibold text-gray-700 mt-4">01</Text>
-              <Text className="text-lg font-semibold text-gray-700 mt-4">Shantha Kumari</Text>
-              <View className="flex-row items-center mt-4 space-x-2">
-    
-                <View className="flex-row items-center">
-                  <Pressable
-                    className="w-5 h-5 border border-gray-400 rounded flex items-center justify-center mr-2"
-                    onPress={() => {/* Add  checkbox toggle logic here */ }}
-                  >
-    
-                  </Pressable>
-    
-                </View>
-              </View>
+      style={{
+        marginTop: 12,
+        width: '100%',
+        paddingHorizontal: 16,
+        paddingVertical: 3,
+        backgroundColor: 'white',
+        borderRadius: 8,
+
+      }}>
+      <Text className="text-lg font-semibold text-gray-700 mt-4">{data.id}</Text>
+      <Text className="text-lg font-semibold text-gray-700 mt-4">{data.name}</Text>
+      <View className="flex-row items-center mt-4 space-x-2">
+
+        <View className="flex-row items-center">
+          <Pressable onPress={() => { }}>
+            <View
+              className={`w-5 h-5 border rounded ${data.present ? 'bg-blue-500' : 'bg-white'}`}
+            />
+          </Pressable>
+
+        </View>
+      </View>
     </View>
   )
 }

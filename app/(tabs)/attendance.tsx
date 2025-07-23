720cf5bd922f4d203f1b94f2d5143a4614b4c29b
tharindu <tharindu.builtapps@gmail.com>
2025-07-23 17:44:01 +0530

    update attendance tab UI

--- app/(tabs)/attendance.tsx
+++ app/(tabs)/attendance.tsx
@@ -33,7 +33,7 @@ const Attendance = () => {
         }}
       >
         <View className='flex-row items-center justify-between'>
-          <Text className="font-bold text-2xl pl-2 pt-4">attendance</Text>
+          <Text style={{ fontFamily: 'Poppins-Bold' }} className="text-2xl pl-2 pt-4">attendance</Text>
 
           <View className="pl-4 items-center mt-4">
             <Pressable
@@ -96,9 +96,9 @@ const Attendance = () => {
             width: '90%',
 
           }}>
-          <Text className="text-lg  text-gray-700 mt-4">No</Text>
-          <Text className="text-lg  text-gray-700 mt-4">Name</Text>
-          <Text className="text-lg  text-gray-700 mt-4">Present/Absent</Text>
+          <Text style={{ fontFamily: 'Poppins' }} className="text-lg  text-gray-700 mt-4">No</Text>
+          <Text style={{ fontFamily: 'Poppins' }} className="text-lg  text-gray-700 mt-4">Name</Text>
+          <Text style={{ fontFamily: 'Poppins' }} className="text-lg  text-gray-700 mt-4">Present/Absent</Text>
         </View>
         {
           employees.map((employee, index) => (
@@ -110,20 +110,20 @@ const Attendance = () => {
             className="bg-gray-400 px-6 py-2 rounded-xl"
             onPress={() => { }}
           >
-            <Text className="text-white font-semibold">Next &gt;&gt;</Text>
+            <Text style={{ fontFamily: 'Poppins' }} className="text-white font-semibold">Next &gt;&gt;</Text>
           </Pressable>
           <View className="flex-row space-x-4 mt-4 ">
             <Pressable
               className="bg-white px-6 py-2 rounded-xl border border-gray-400"
               onPress={() => { }}
             >
-              <Text className="text-green-800 font-semibold">Cancel</Text>
+              <Text style={{ fontFamily: 'Poppins' }} className="text-green-800 font-semibold">Cancel</Text>
             </Pressable>
             <Pressable
               className="bg-green-800 px-6 py-2 rounded-xl ml-8 "
               onPress={() => { }}
             >
-              <Text className="text-white font-semibold">Save Changes</Text>
+              <Text style={{ fontFamily: 'Poppins' }} className="text-white font-semibold">Save Changes</Text>
             </Pressable>
           </View>
         </View>
@@ -138,6 +138,7 @@ const Attendance = () => {
           <View className='flex-row justify-between'>
             <Text
               style={{
+                fontFamily: 'Poppins',
                 fontSize: 14,
                 fontWeight: 'bold',
                 marginBottom: 8,
@@ -147,7 +148,7 @@ const Attendance = () => {
               Total Employee Count
             </Text>
             <View className="border-2 border-gray-400 bg-gray-100 rounded-2xl px-4 py-2 mt-2 mr-8">
-              <Text>
+              <Text style={{fontFamily: 'Poppins'}} className="text-gray-700 text-lg font-semibold">
                 12
               </Text>
             </View>
@@ -155,15 +156,15 @@ const Attendance = () => {
 
           <View>
             <View className="flex-row border-2 border-gray-400 bg-gray-100 rounded-2xl px-4 py-4 mt-2 justify-between items-center">
-              <Text className="text-green-700 text-lg font-semibold">Present Count</Text>
+              <Text style={{fontFamily: 'Poppins'}} className="text-green-700 text-lg font-semibold">Present Count</Text>
               <View style={{ width: 2, height: 32, backgroundColor: '#747474' }} />
-              <Text className="text-green-700 text-lg font-semibold">10</Text>
+              <Text style={{fontFamily: 'Poppins'}} className="text-green-700 text-lg font-semibold">10</Text>
             </View>
 
             <View className="flex-row border-2 border-gray-400 bg-gray-100 rounded-2xl px-4 py-4 mt-2 justify-between items-center">
-              <Text className="text-green-700 text-lg font-semibold">Absent Count</Text>
+              <Text style={{fontFamily: 'Poppins'}} className="text-green-700 text-lg font-semibold">Absent Count</Text>
               <View style={{ width: 2, height: 32, backgroundColor: '#747474' }} />
-              <Text className="text-red-700 text-lg font-semibold">02</Text>
+              <Text style={{fontFamily: 'Poppins'}} className="text-red-700 text-lg font-semibold">02</Text>
             </View>
           </View>
         </View>
